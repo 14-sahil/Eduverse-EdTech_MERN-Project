a7acc50f3fed6495b0ae49735cf61a8b526a681c
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2025-11-03 15:40:11 +0530

    Update useOnClickOutside.js

--- frontend/src/hooks/useOnClickOutside.js
+++ frontend/src/hooks/useOnClickOutside.js
@@ -9,7 +9,7 @@ export default function useOnClickOutside(ref, handler) {
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
-      // Otherwise, call the provided handler function
+      // Otherwise, call the provided handler function 
       handler(event);
     };
 
